18e764449e061f79d41483949b4baf88a3916a56
aichnas <127129419+aichnas@users.noreply.github.com>
2023-05-05 20:27:23 +0100

    JS in correct file

---
+++ NineLives/JS/java.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementById(`toggleButton`);    
+const naviList =  document.getElementById(`navi-list`);
+
+toggleButton.addEventListener(`click`, () => {
+    naviList.classList.toggle(`active`);
+
+})
